6594929755b82266db7bcc947482dcf00f988ffc
ManeeshDev <manishprashangamage23@gmail.com>
2022-12-09 09:50:51 +0530

    api related changes

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "coding_challenge",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASS,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DIALECT,
   pool: {
     max: 5,
     min: 0,
