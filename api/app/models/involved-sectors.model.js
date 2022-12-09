dce56f2243d134cecfaaf3ea44a6b2f64e524ad2
ManeeshDev <manishprashangamage23@gmail.com>
2022-12-09 13:05:34 +0530

    api related changes

--- api/app/models/involved-sectors.model.js
+++ api/app/models/involved-sectors.model.js
@@ -4,5 +4,24 @@ module.exports = (sequelize, Sequelize) => {
 		sectors: { type: Sequelize.STRING },
 		isAgreedToTerms: { type: Sequelize.INTEGER },
 	});
+
+	(async () => {
+		await InvolvedSector.sync();
+		const {count, rows} = await InvolvedSector.findAndCountAll();
+		if (count === 0 && rows.length === 0) {
+			console.log("===================================InvolvedSector bulkCreate======================");
+			InvolvedSector.bulkCreate([
+				{name: 'Maneesh', sectors: '[121,28,35,122,581,6]', isAgreedToTerms: 1 },
+				{name: 'Shalini', sectors: '[6,40,43,390,98,18]', isAgreedToTerms: 1 },
+				{name: 'Yashika', sectors: '[101,392,224,91,341]', isAgreedToTerms: 1 },
+				{name: 'Prashantha', sectors: '[33,51,263]', isAgreedToTerms: 1 },
+				{name: 'Kavindya', sectors: '[42,141,342,113]', isAgreedToTerms: 1 },
+			]).then(() => {
+				console.log("InvolvedSector bulkCreate done..");
+				console.log("===================================InvolvedSector bulkCreate======================");
+			});
+		}
+	})();
+
 	return InvolvedSector;
 };
--- api/app/models/user.model.js
+++ api/app/models/user.model.js
@@ -3,5 +3,20 @@ module.exports = (sequelize, Sequelize) => {
 		email: { type: Sequelize.STRING },
 		password: { type: Sequelize.STRING },
 	});
+
+	(async () => {
+		await User.sync();
+		const {count, rows} = await User.findAndCountAll();
+		if (count === 0 && rows.length === 0) {
+			console.log("===================================User bulkCreate======================");
+			User.bulkCreate([
+				{email: 'maneesh@gmail.com', password: '&yqpXPwHDKZVr2?' },
+			]).then(() => {
+				console.log("User bulkCreate done..");
+				console.log("===================================User bulkCreate======================");
+			});
+		}
+	})();
+
 	return User;
 };
