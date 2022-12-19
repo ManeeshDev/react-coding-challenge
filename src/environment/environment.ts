241348211f0bb977a59d601f02fe2ed7630e65bb
ManeeshDev <manishprashangamage23@gmail.com>
2022-12-20 01:26:48 +0530

    env related

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    api_url: "http://localhost:8080",
+    // api_url: "http://localhost:8080",
+    api_url: "https://a1d4-2402-d000-a400-84f-3875-d698-632d-9e24.ap.ngrok.io",
 };
